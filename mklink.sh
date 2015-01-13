01ad4ed01dd4c1602f851b6a6e35d0facf57e0e4
Kurt Wolf <itguy614@gmail.com>
2015-01-13 14:32:02 -0500

    Updating come git_bash_prompt configs and global editor config

--- mklink.sh
+++ mklink.sh
@@ -3,3 +3,5 @@
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.editorconfig ~/.editorconfig
+ln -s ~/dotfiles/.hushlogin ~/.hushlogin
