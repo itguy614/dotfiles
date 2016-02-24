9dd26e3c8bffef6085682e6d78a128934404f24f
Kurt Wolf <itguy614@gmail.com>
2016-02-24 07:34:57 -0500

    Adding global ctags config

--- mklink.sh
+++ mklink.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 rm ~/.bashrc
-ln -s ~/dotfiles/.bash_profile ~/.bashrc
 
+ln -s ~/dotfiles/.ansible ~/.ansible.cfg
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.bash_profile ~/.bashrc
+ln -s ~/dotfiles/.ctags ~/.ctags
+ln -s ~/dotfiles/.editorconfig ~/.editorconfig
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -s ~/dotfiles/.editorconfig ~/.editorconfig
 ln -s ~/dotfiles/.hushlogin ~/.hushlogin
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.ansible ~/.ansible.cfg
-ln -s ~/dotfiles/.vim/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.vim/.gvimrc ~/.gvimrc
 ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vim/.gvimrc ~/.gvimrc
+ln -s ~/dotfiles/.vim/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
